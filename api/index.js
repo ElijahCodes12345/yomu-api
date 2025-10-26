c4262551eeb6c32787359f5b695a6aa328128664
ElijahCodes12345 <oluwafemiadebanwo@gmail.com>
2025-10-26 01:09:21 +0100

    vercel compability update

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const mangaPillRoutes = require('./routes/mangaPill');
+const mangaPillRoutes = require('../routes/mangaPill');
 
 const app = express();
 
